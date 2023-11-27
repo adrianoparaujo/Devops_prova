87df29787a35f4d67bd32948595facec0950fd9a
adrianoparaujo <111307893+adrianoparaujo@users.noreply.github.com>
2023-11-27 19:10:14 -0300

    teste.py

--- app.py
+++ app.py
@@ -2,18 +2,46 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/Home_page')
-def Home_Page():
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+@app.route('/index')
+def index():
+    return '''
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Max Solaris</title>
+    <link rel="stylesheet" href="style.css">
+      
+</head>
+<body>
+    <div class="fundo">
+        <nav class="barranave">
+            <a href="index.html">  <img class="logo" src="img/logo.png" > </a>
+            <ul>
+                <li><a href="historia.html"> Historia </a> </li>
+                <li><a href="formacao.html"> Formação </a> </li>    
+                <li><a href="projetos.html"> Projetos </a> </li>
+                <li><a href="premios.html"> Prêmios </a> </li>  
+                <li><a href="Contato.html"> Contato </a> </li>                  
+            </ul>
+        </nav>
+        <div class="CentroTexto">
+            <h1> Max Solaris </h1>
+            <h2> Aventureiro Espacial </h2>
 
-    index_path = os.path.join(current_directory, 'index.html')
+        </div>
 
-    if os.path.exists(index_path):
-        with open(index_path, 'r') as file:
-            content = file.read()
-        return content
-    else:
-        return "Erro: arquivo 'index.html' não encontrado."
+        <div class="resumo">
+            <h1> Max Solaris é um renomado astronauta conhecido por suas incríveis habilidades.</h1><br/>
+            <h2> Desde muito jovem demonstrou uma paixão ardente pelo cosmos.</h2><br/>
+            <h3> Sua jornada rumo às estrelas começou com uma sólida formação educacional.</h3>  
+             
+        </div>
 
-if __name__ == '__main__':
-    app.run()
+    </div> 
+</body>
+</html>
+
+
+'''
