61a4b3950344d159c5eb76c4c59911578dc82b3d
adrianoparaujo <111307893+adrianoparaujo@users.noreply.github.com>
2023-11-27 20:26:44 -0300

    dev05 app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/historia')
+def historia():
     return '''
 <!DOCTYPE html>
 <html lang="pt-BR">
