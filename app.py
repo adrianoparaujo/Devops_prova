f099322fc8b7ece93fb97b9d8c0d969f6b010fba
adrianoparaujo <111307893+adrianoparaujo@users.noreply.github.com>
2023-11-27 21:38:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/Home_page')
 def Home_Page():
     current_directory = os.path.dirname(os.path.abspath(__file__))
 
